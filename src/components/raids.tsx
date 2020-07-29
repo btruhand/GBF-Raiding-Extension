0a71f1aeed7fa46ab7587c0da981a338f9258773
btruhand <btara.truhandarien@gmail.com>
2020-07-29 01:54:38 -0700

    add simple on and off state change when raid display clicked

--- src/components/raids.tsx
+++ src/components/raids.tsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RaidBoss } from '@/lib/raids';
 import styles from '@/styles/raids.module.scss';
+
+function RaidDisplay(props: { boss: RaidBoss }) {
+    const [on, setOn] = useState(false)
+    const computedOnClass = on ? styles['raid-on'] : ''
+    return <div key={props.boss.uniqueName()} className={`${styles['raid-info']} ${computedOnClass}`}
+        onClick={() => setOn(!on)}>
+        <p>{props.boss.englishName()}</p>
+        <p>{props.boss.japaneseName()}</p>
+    </div>
+}
 
 function RaidsList(props: { difficulty: string, data: RaidBoss[] }) {
     return (
         <div>
             <p>{props.difficulty}</p>
             <div className={styles['list-raids']}>
-                {props.data.map(boss =>
-                    <div key={boss.uniqueName()} className={styles['raid-info']}>
-                        <p>{boss.englishName()}</p>
-                        <p>{boss.japaneseName()}</p>
-                    </div>
-                )}
+                {props.data.map(boss => <RaidDisplay boss={boss} />)}
             </div>
         </div>
     )
